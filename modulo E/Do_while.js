82d3e077d18186ebc5f807ee8fc15b4c9ec05366
wallace azevedo <74993539+wallaceAzevedo@users.noreply.github.com>
2022-08-09 08:24:39 -0300

    do e while

---
+++ modulo E/Do_while.js
@@ -0,0 +1,10 @@
+// estrutura de reoeticao do final para o inicio
+
+// ira repetir enquanto C for menor ou igual a 6
+
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6 )
---
+++ modulo E/while.js
@@ -0,0 +1,10 @@
+// estrutura de reoeticao no inicio
+
+// ira repetir enquanto c for menor ou igual a 6
+
+var c = 1
+
+while (c <= 6 ){
+    console.log(`Passo ${c}`)
+    c++
+}
